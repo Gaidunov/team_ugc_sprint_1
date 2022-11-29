ad877d242dc583e21267d795c6f9a6063166cdf4
kbondar17 <ironbondar@gmail.com>
2022-11-29 21:40:09 +0300

    added fake data

--- src/api/api.py
+++ src/api/api.py
@@ -2,16 +2,19 @@ from fastapi import FastAPI
 from http import HTTPStatus
 
 from src.api.models import MovieTimeStamp
-from src.kafka.kafka_producer import kafka
-
-
-app = FastAPI(docs_url='/docs')
+from src.my_kafka.kafka_producer import kafka
 
  
+app = FastAPI(docs_url='/docs')
+
+
 @app.post("/send_stamp")
 async def send_time_stamp(data:MovieTimeStamp):
     """отправляем в кафку инфу о прогрессе просмотра фильма юзером"""
-    key = str(data.user_id).encode('utf-8') + '+'.encode('utf-8') + str(data.movie_id).encode('utf-8')
+
+    key = str(data.user_id).encode('utf-8') + '+'.encode('utf-8') + \
+            str(data.movie_id).encode('utf-8') 
+            
     value = str(data.timestamp).encode('utf-8')
     kafka.send(key=key, value=value)
     return HTTPStatus.OK
--- src/api/fake_views.py
+++ src/api/fake_views.py
@@ -1,25 +1,29 @@
 import random
+import json
 import asyncio
-import json 
 
 import aiohttp
 
 from src.api.models import MovieTimeStamp
 
+with open('movies_with_time.json', 'r', encoding='utf-8') as f:
+    movies = json.load(f)
+
+users = [e for e in range(1000)]
+
 
 async def test_api_kafka():
     '''симулируем просмотр фильмов множеством юзеров'''
-
+    
     async with aiohttp.ClientSession() as session:
         for _ in range(1000):
-            request_body = MovieTimeStamp(
-                    movie_id=random.randint(1, 1000),
-                    user_id=random.randint(1, 1000),
-                    timestamp=random.randint(1, 14400)
-                )
+            movie_id, _, timestamp = random.choice(movies)
+            user_id = random.choice(users)
+            request_body = MovieTimeStamp(user_id=user_id,
+                                          movie_id=movie_id,
+                                          timestamp=timestamp)
             async with session.post('http://127.0.0.1:8000/send_stamp', json=request_body.dict()) as resp:
                 print(resp.status)
-        
-
+            
 asyncio.run(test_api_kafka())
 
