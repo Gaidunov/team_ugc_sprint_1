81420f3131237f1634c72446b6e508e78d9f8274
kbondar17 <ironbondar@gmail.com>
2022-12-08 17:49:34 +0300

    fix api

--- src/api/models.py
+++ src/api/models.py
@@ -4,5 +4,6 @@ class MovieTimeStamp(BaseModel):
     user_id: str
     movie_id: str
     timestamp: int
+    movie_duration:int
 
-MovieTimeStamp(user_id=1, movie_id=1, title='1', timestamp=1)
+ms = MovieTimeStamp(user_id=1, movie_id=1, title='1', timestamp=1, movie_duration=10)
